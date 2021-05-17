ce8b94c085357b37323bc6207dadaf47b4744fac
Yi Long <peng@M1.com>
2021-05-17 08:01:50 +0800

    add conditional course

--- 0211BasicVariables/Program.cs
+++ 0211BasicVariables/Program.cs
@@ -7,36 +7,24 @@ namespace _0211BasicVariables
         static void Main(string[] args)
         {
             string firstName = "";
-            string lastName = "";
-            string fullName = "";
-
-            firstName = "Tim";
-            lastName = "Corey";
-            // $ is for string interpolation
-            fullName = $"{ firstName } { lastName }";
             
+            Console.Write("What is your first name: ");
+            firstName = Console.ReadLine();
             
-            string fileName ="";
-            // @ is for string literals
-            fileName = @"C:\Temp|temp.txt";
-
-            // Whole number
-            int age = 0;
-            age = 25;
-
-            string zipCode = "00411-4125";
-            System.Console.WriteLine(zipCode.ToString());
+            // if ( firstName.ToLower() == "long")
+            // {
+            //     Console.WriteLine("Welcome Mr. Yi");
+            // }
             
-            // Use double for all numbers that include fractions except for money
-            double averageWordsPerMinute = 34.24;
-            // Use decimal for money
-            decimal costPerContainer = 43.85M;
+            if ( firstName.ToLower() == "long")
+            {
+                Console.WriteLine("Hello, Mr. Yi");
+            }else{
+                Console.WriteLine("Hello other person.");
+            }
 
-            // Stores only true or false
-            bool isAlive = false;
+            Console.WriteLine("Application Done.");
 
-
-            System.Console.WriteLine(age);
             Console.ReadLine();
         }
     }
---
+++ 0212Conditionals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0212Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
