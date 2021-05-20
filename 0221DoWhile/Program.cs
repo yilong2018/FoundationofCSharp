7b36662c1cfeae93aa25c50f0e22347b1a860f32
Yi Long <peng@M1.com>
2021-05-20 08:07:33 +0800

    add array course

--- 0221DoWhile/Program.cs
+++ 0221DoWhile/Program.cs
@@ -6,7 +6,70 @@ namespace _0221DoWhile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // //Do loop
+            // string isContinue = "";
+            // do
+            // {
+            //     System.Console.Write("What's yoru name? ");
+            //     string userName = Console.ReadLine();
+            //     Console.WriteLine($"Hello, welcome { userName }");
+
+            //     Console.Write("Do you want to continue?(yes/no) ");
+            //     isContinue = Console.ReadLine();
+            // } while ( isContinue.ToLower() == "yes" );
+
+            // // While loop
+            // Console.Write("What is your age? ");
+            // string ageText = Console.ReadLine();
+            // bool isValidAge = int.TryParse(ageText, out int age);
+
+            // while ( isValidAge == false )
+            // {
+            //     Console.WriteLine("The age you input isn't a number, please try again.");
+            //     Console.Write("What's your age? ");
+            //     ageText = Console.ReadLine();
+            //     isValidAge = int.TryParse(ageText, out age);
+            // }
+
+            // Console.WriteLine($"Your age in 10 years will be { age + 10 }.");
+
+
+            // Homework
+            // Do loop
+            // string isExit = "";
+            // do
+            // {
+            //     Console.Write("What is your name? ");
+            //     string userName = Console.ReadLine();
+            //     if ( userName.ToLower() == "tim" )
+            //     {
+            //         Console.WriteLine("Hello Professor.");
+            //     }else
+            //     {
+            //         Console.WriteLine($"Hi { userName }");
+            //     }
+            //     Console.Write("Do you want to exit? (exit/not)");
+            //     isExit = Console.ReadLine();
+            // } while ( isExit.ToLower() != "exit" );
+
+            // While loop
+            string isExit = "";
+            while (isExit.ToLower() != "exit")
+            {
+                Console.Write("What is your name? ");
+                string userName = Console.ReadLine();
+                if (userName.ToLower() == "tim")
+                {
+                    Console.WriteLine("Hello Professor.");
+                }
+                else
+                {
+                    Console.WriteLine($"Hi { userName }");
+                }
+                Console.Write("Do you want to exit? (exit/no)");
+                isExit = Console.ReadLine();
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ Moudule02Lesson07/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Moudule02Lesson07
+{
+    class Program
+    {
+        // Mainapp.exe /p /r /t /text /23 /34
+        static void Main(string[] args)
+        {
+            // string[] firstNames = new string[5]; // 0, 1, 2, 3, 4
+
+            // firstNames[0] = "Tim";
+            // firstNames[1] = "Sue";
+            // firstNames[2] = "Bod";
+            // firstNames[4] = "Carl";
+
+            // System.Console.WriteLine($"My array has {firstNames[0]}, {firstNames[1]}, {firstNames[2]}, {firstNames[4]}.");
+
+            // string data = "Tim,Sue,Bob,Frank,Sandra,Molly";
+            // string[] firstNames = data.Split(',');
+
+            // Console.WriteLine($"The third first name is { firstNames[2] }.");
+            // Console.WriteLine($"The third first name is { firstNames[6] }.");
+
+            // int[] ages = new int[3];
+            // ages[0] = 4;
+            // ages[1] = 5;
+            // ages[2] = 54;
+
+            // string[] lastNames = new string[]{ "Corey,", "Smith", "Jones" };
+
+            // lastNames[1] = "Malcome";
+
+
+            // Homework, Create an array of 3 names. Ask the user which number to select. When the user gives you a number, return that name.
+            string[] userNames = new string[] { "Bob", "Sue", "Smith"};
+            Console.WriteLine("We have three names, pick up one using number:")
+
+            Console.ReadLine();
+        }
+    }
+}
