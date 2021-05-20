c319d110073d4469e10e4eac596e864a4de3c01a
yilong <yilong2018@gmail.com>
2021-05-20 13:45:27 +0800

    add list course

---
+++ Module02Lesson07/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Moudule02Lesson07
+{
+  class Program
+  {
+    // Mainapp.exe /p /r /t /text /23 /34
+    static void Main(string[] args)
+    {
+      // string[] firstNames = new string[5]; // 0, 1, 2, 3, 4
+
+      // firstNames[0] = "Tim";
+      // firstNames[1] = "Sue";
+      // firstNames[2] = "Bod";
+      // firstNames[4] = "Carl";
+
+      // System.Console.WriteLine($"My array has {firstNames[0]}, {firstNames[1]}, {firstNames[2]}, {firstNames[4]}.");
+
+      // string data = "Tim,Sue,Bob,Frank,Sandra,Molly";
+      // string[] firstNames = data.Split(',');
+
+      // Console.WriteLine($"The third first name is { firstNames[2] }.");
+      // Console.WriteLine($"The third first name is { firstNames[6] }.");
+
+      // int[] ages = new int[3];
+      // ages[0] = 4;
+      // ages[1] = 5;
+      // ages[2] = 54;
+
+      // string[] lastNames = new string[]{ "Corey,", "Smith", "Jones" };
+
+      // lastNames[1] = "Malcome";
+
+
+      // Homework, Create an array of 3 names. Ask the user which number to select. When the user gives you a number, return that name.
+      string[] userNames = new string[] { "Bob", "Sue", "Smith" };
+      Console.WriteLine("We have three names: ");
+      System.Console.WriteLine($"[1] { userNames[0] }");
+      System.Console.WriteLine($"[2] { userNames[1] }");
+      System.Console.WriteLine($"[3] { userNames[2] }");
+      Console.Write("Pick up one using number:");
+      string numberText = Console.ReadLine();
+      bool numberIsValid = int.TryParse(numberText, out int number);
+      while (numberIsValid == false || number > 3 || number < 1)
+      {
+        System.Console.Write("Wrong Number, input the right number:");
+        numberText = Console.ReadLine();
+        numberIsValid = int.TryParse(numberText, out number);
+      }
+      System.Console.WriteLine($"You picked { userNames[number - 1] }");
+
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Module02Lesson08List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module02Lesson08List
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // List<string> firstNames = new List<string>();
+      // firstNames.Add("Tim");
+      // firstNames.Add("Sue");
+      // firstNames.Add("Bob");
+
+      // Console.WriteLine($"The second element is { firstNames[1] }");
+
+      // firstNames.Remove("Sue");
+      // Console.WriteLine($"The second element is { firstNames[1] }");
+
+      // firstNames[1] = firstNames[1].ToUpper();
+      // Console.WriteLine($"The second element is { firstNames[1] }");
+
+      // Homework
+      // Add students to a class roster until there are no more students. Then print out the count of the sutdents to the Console.
+
+      int studentsCount = 0;
+      string studentName = "";
+      string quitInput = "";
+      List<string> roster = new List<string>();
+      //add students to roster
+      while (quitInput.ToUpper() != "NO")
+      {
+        System.Console.Write("Input a student name: ");
+        studentName = Console.ReadLine();
+        roster.Add(studentName);
+        studentsCount++;
+        System.Console.Write("Do you want add mrore student? [yes/no]: ");
+        quitInput = Console.ReadLine();
+      }
+
+      // print count of students
+      System.Console.WriteLine($"We have { studentsCount } students in roster.");
+      Console.ReadLine();
+    }
+  }
+}
