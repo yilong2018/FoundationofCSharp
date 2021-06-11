3a2a6a1cfc2e0d234c9e656a86071fc4cac92aa9
Yi long <yilong@vmvirtual01.com>
2021-06-11 22:29:16 +0800

    Start M08L05SqlContactsDB

---
+++ M08L05RelationalDBSolution/SQLContactsDB/dbo/Tables/ContactEmail.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ContactEmail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ContactId] INT NOT NULL, 
+    [EmailAddressId] INT NOT NULL,
+
+)
---
+++ M08L05RelationalDBSolution/SQLContactsDB/dbo/Tables/PhoneNumbers.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PhoneNumbers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PhoneNumber] VARCHAR(20) NOT NULL   -- 212-555-1212 x123
+)
